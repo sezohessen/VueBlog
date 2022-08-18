1c57ac983c10429f182f855cc49de5d46bc743d0
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-19 00:00:08 +0200

    Refs for reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
    <h1>Home</h1>
-   <p ref="p">My name is {{name}}</p>
+   <p ref="p">My name is {{name}} and my age is {{age}}</p>
    <button @click="handleClick">Click me</button>
+   <button @click="age++">Increase Age</button>
+   <input type="text" v-model="name">
   </div>
 </template>
 
@@ -14,12 +16,14 @@ export default {
 
     console.log('setup')
     const p = ref(null)
-    let name = 'maro'
-    let age = 14
+    const name = ref('maro')
+    const age = ref(14)
     const handleClick = () =>{
       console.log(p,p.value)
       p.value.classList.add('test')
-      p.value.textContent = 'Hello From Setup'
+      /* p.value.textContent = 'Hello From Setup' */
+      name.value = 'Sezo'
+      age.value = 23
     }
     return {name,age,handleClick,p}
   },
