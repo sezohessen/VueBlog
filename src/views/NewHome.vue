c9b4ab534ac847f2cf5e4df3d1df560e8bfa3140
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-19 05:40:07 +0200

    Fetching data from setup

--- src/views/NewHome.vue
+++ src/views/NewHome.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="TogglePost" :posts="posts"/>
-    <button @click="TogglePost = !TogglePost">Toggle post</button>
-    <button @click="posts.pop()">Delete Post</button>
+    <div v-if="error">{{error}}</div>
+    <div v-if="posts.length">
+        <PostList :posts="posts"/>
+    </div>
+    <div v-else>
+        Loading....
+    </div>
   </div>
 </template>
 <script>
@@ -13,12 +17,22 @@ export default {
   name: 'HomeView',
   components:{PostList},
   setup() {
-      const posts = ref([
-          {title:'title one',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat obcaecati repellendus, tempora odio molestias corporis, laborum maiores nostrum atque, quod perferendis? Deserunt iure qui vitae expedita quasi aperiam, dolore blanditiis ipsa sed doloribus enim vel corrupti maxime ex commodi voluptatum perferendis ipsam deleniti officiis quos! Voluptas consectetur laudantium rerum dolores non eveniet magni quos ex distinctio quam perferendis cum mollitia vel delectus ducimus, explicabo accusantium quasi ab enim quisquam aliquid laborum voluptatum quae. Iure inventore ratione exercitationem! Quibusdam soluta tempora pariatur suscipit iste, quos ex ullam nobis doloribus odit adipisci porro? Tempore, magnam? Qui nihil dolor distinctio deleniti, repellat voluptas!',id:1},
-          {title:'title two',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus aspernatur quasi nulla qui quia tempora eaque obcaecati culpa. Pariatur commodi fuga beatae officiis alias consequatur quidem cum facere animi ipsam.',id:2}
-      ])
-      const TogglePost = ref(true)
-    return {posts,TogglePost}
+      const posts = ref([])
+      const error = ref(null)
+      const load = async () => {
+        try {
+            let data = await fetch('http://localhost:3000/posts')
+            if(!data.ok){
+                throw Error('No data Available')
+            }
+            posts.value = await data.json()
+        } catch (err) {
+            error.value = err.message
+            console.log(error.value)
+        }
+      }
+      load()
+    return {posts,error}
   },
 }
 </script>
