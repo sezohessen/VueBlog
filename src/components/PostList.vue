58d26dd26de1419a8f8c64034438c15c273cc53f
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-19 05:20:37 +0200

    Life Cycle Hooks

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="post-list">
       <div v-for="post in posts" :key="post.id">
-        <SinglePost :post="post"/>
+        <SinglePost :post="post"/>  
       </div>
   </div>
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
 import SinglePost from './SinglePost.vue'
 export default {
     props:['posts'],
     components:{SinglePost},
     setup(props){
-        console.log
+        onMounted(()=> console.log('Component Mounted'))
+        onUpdated(()=> console.log('Component Updated'))
+        onUnmounted(()=> console.log('Component UnMounted'))
     }
 }
 </script>
--- src/views/NewHome.vue
+++ src/views/NewHome.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts"/>
+    <PostList v-if="TogglePost" :posts="posts"/>
+    <button @click="TogglePost = !TogglePost">Toggle post</button>
+    <button @click="posts.pop()">Delete Post</button>
   </div>
 </template>
 <script>
@@ -15,7 +17,8 @@ export default {
           {title:'title one',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat obcaecati repellendus, tempora odio molestias corporis, laborum maiores nostrum atque, quod perferendis? Deserunt iure qui vitae expedita quasi aperiam, dolore blanditiis ipsa sed doloribus enim vel corrupti maxime ex commodi voluptatum perferendis ipsam deleniti officiis quos! Voluptas consectetur laudantium rerum dolores non eveniet magni quos ex distinctio quam perferendis cum mollitia vel delectus ducimus, explicabo accusantium quasi ab enim quisquam aliquid laborum voluptatum quae. Iure inventore ratione exercitationem! Quibusdam soluta tempora pariatur suscipit iste, quos ex ullam nobis doloribus odit adipisci porro? Tempore, magnam? Qui nihil dolor distinctio deleniti, repellat voluptas!',id:1},
           {title:'title two',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus aspernatur quasi nulla qui quia tempora eaque obcaecati culpa. Pariatur commodi fuga beatae officiis alias consequatur quidem cum facere animi ipsam.',id:2}
       ])
-    return {posts}
+      const TogglePost = ref(true)
+    return {posts,TogglePost}
   },
 }
 </script>
