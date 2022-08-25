c9295b36a689f90268370a46d8c9b8cb9ab0925f
sezohessen <hu.elshazly@nu.edu.eg>
2022-08-25 21:21:50 +0200

    Creating web form

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,34 +1,94 @@
 <template>
   <div class="create">
-      <form action="">
-          <label>Title:</label>
-          <input v-model="title" type="text" required>
-          <label>Content:</label>
-          <textarea v-model="body" required></textarea>
-          <label>Tags (hit enter to add a tag)</label>
-          <input 
-          v-model="tag" 
-          type="text"
-          @keydown.enter.prevent=""
-          >
-          <button type="submit">Add Post</button>
-      </form>
+    <form action="">
+      <label>Title:</label>
+      <input v-model="title" type="text" required />
+      <label>Content:</label>
+      <textarea v-model="body" required></textarea>
+      <label>Tags (hit enter to add a tag)</label>
+      <input v-model="tag" type="text" @keydown.enter.prevent="handelKeyDown" />
+      <div v-for="tag in tags" :key="tag" class="pill">
+        #{{ tag }}
+      </div>
+      <button type="submit">Add Post</button>
+    </form>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { ref } from "@vue/reactivity";
 export default {
-    setup(){
-        const title = ref('')
-        const body = ref('')
-        const tag = ref('')
-        const tags = ref([])
-        return {title,body,tag}
-    }
-}
+  setup() {
+    const title = ref("");
+    const body = ref("");
+    const tag = ref("");
+    const tags = ref([]);
+    const handelKeyDown = () => {
+      if (!tags.value.includes(tag.value)) {
+        tag.value = tag.value.replace("/s/", ""); //Remove white space
+        tags.value.push(tag.value);
+      }
+      tag.value = ''
+    };
+    return { title, body, tag, handelKeyDown,tags };
+  },
+};
 </script>
 
 <style>
-
+form {
+    max-width: 480px;
+    margin: 0 auto;
+    text-align: left;
+  }
+  input, textarea {
+    display: block;
+    margin: 10px 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    border: 1px solid #eee;
+  }
+  textarea {
+    height: 160px;
+  }
+  label {
+    display: inline-block;
+    margin-top: 30px;
+    position: relative;
+    font-size: 20px;
+    color: white;
+    margin-bottom: 10px;
+  }
+  label::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: #ff8800;
+    position: absolute;
+    z-index: -1;
+    padding-right: 40px;
+    left: -30px;
+    transform: rotateZ(-1.5deg);
+  }
+  button {
+    display: block;
+    margin-top: 30px;
+    background: #ff8800;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    font-size: 18px;
+    cursor: pointer;
+  }
+  .pill {
+    display: inline-block;
+    margin: 10px 10px 0 0;
+    color: #444;
+    background: #ddd;
+    padding: 8px;
+    border-radius: 20px;
+    font-size: 14px;
+  }
 </style>
